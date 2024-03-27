f5383bd1c70a74ea45ebfc109846fa786506b2a2
Medigone <admin@medigo.one>
2024-03-27 17:46:14 +0100

    new doctypes

---
+++ archigo/archigo/doctype/type_opportunite/type_opportunite.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MEDIGO ONE and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Type Opportunite", {
+// 	refresh(frm) {
+
+// 	},
+// });
